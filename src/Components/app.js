f65e11d12f380a48864da967aff5d3a50447ccda
Brady <bradyrcotton@gmail.com>
2021-04-29 14:24:32 -0500

    added search result folder and file

--- src/Components/app.js
+++ src/Components/app.js
@@ -2,13 +2,15 @@ import React, {Component} from 'react';
 import SearchBar from './SearchBar/searchBar';
 import Video from './Video/video';
 import RecommendedVideos from './RecommendedVideos/recommendedVideos';
+import SearchResults from '../Components/SearchResults/searchResults';
 import axios from 'axios'
 
 
 class App extends Component {
     state = {
         userinput: '',
-        video: []
+        videos: [],
+        video: {}
     }
 
 
@@ -19,10 +21,21 @@ componentDidMount(){
 
 async searchQuery(video){
     console.log(video)
-    return await axios.get(`https://www.googleapis.com/youtube/v3/search?q='${video}'&key=AIzaSyBwIiD2pkn5uOiRp8ZH3XfaLJ0qQdwyy6Q`)
+    let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q='${video}'&key=AIzaSyBwIiD2pkn5uOiRp8ZH3XfaLJ0qQdwyy6Q`);
+    this.setState({
+        videos: response.data
+    })
+    
 }
 
-
+mapVideos(){
+    return this.state.videos.map(video =>
+        <Video
+        key={video.id}
+        video={video}
+        />
+        )
+}
 
 
 
@@ -31,8 +44,9 @@ render(){
     return(
         <div className="container=fluid">
             <SearchBar searchQuery={this.searchQuery.bind(this)}/>
-            <Video />
+            <Video mapVideos={() => this.mapVideos()}/>
             <RecommendedVideos />
+            <SearchResults mapVideos={() => this.mapVideos()}/>
 
         </div>
     );
